25228460eb20481170aca8c7f5968a0bc039c6e2
oluomojossy <oluomojossy@gmail.com>
2024-02-26 21:40:38 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import LoginApi from './Login'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="body">
+      <LoginApi/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Login.jsx
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from 'react';
+import "./App.css"
+
+const LoginApi = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const validateForm = () => {
+    let isValid = true;
+    const newErrors = {};
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!formData.email.trim() || !emailRegex.test(formData.email)) {
+      newErrors.email = 'Valid email is required';
+      isValid = false;
+    }
+
+    if (!formData.password.trim()) {
+      newErrors.password = 'Password is required';
+      isValid = false;
+    }
+
+    setErrors(newErrors);
+    return isValid;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validateForm()) {
+      console.log('Form submitted:', formData);
+    } else {
+      console.log('Form has errors. Please correct them.');
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  useEffect(() => {
+    validateForm();
+  }, [formData]);
+
+  return (
+    <form onSubmit={handleSubmit}>
+        <h1 className='TEXT'>LOGIN</h1>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        {errors.email && <p className="error" style={{color:"red"}}>{errors.email}</p>}
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+        {errors.password && <p className="error" style={{color:"red"}}>{errors.password}</p>}
+      </div>
+      <button type="submit">Log In</button>
+    </form>
+  );
+};
+
+export default LoginApi;
